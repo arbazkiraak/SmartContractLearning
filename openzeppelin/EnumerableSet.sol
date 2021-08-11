2b81d29643c6f308fabbbe9b4897d1f9858f9a03
Arbaz Hussain <13177578+arbazkiraak@users.noreply.github.com>
2021-08-11 17:00:22 +0530

    Create EnumerableSet.sol

---
+++ openzeppelin/EnumerableSet.sol
@@ -0,0 +1,121 @@
+pragma solidity 0.8.0;
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/utils/structs/EnumerableSet.sol";
+
+/*
+
+1. Deploy: `EnumerableAddressSetMock`
+2. add a address ==> bool
+3. check length ==> uint256
+4. contains (address) ==> bool
+5. remove a address => bool
+6. same address can't be added , as it already exists.
+7. at(uint index) ==> address , ex: 0 for first added address
+8. values() ==> list [] of addresses added.
+
+*/
+
+
+// Bytes32Set
+contract EnumerableBytes32SetMock {
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+
+    event OperationResult(bool result);
+
+    EnumerableSet.Bytes32Set private _set;
+
+    function contains(bytes32 value) public view returns (bool) {
+        return _set.contains(value);
+    }
+
+    function add(bytes32 value) public {
+        bool result = _set.add(value);
+        emit OperationResult(result);
+    }
+
+    function remove(bytes32 value) public {
+        bool result = _set.remove(value);
+        emit OperationResult(result);
+    }
+
+    function length() public view returns (uint256) {
+        return _set.length();
+    }
+
+    function at(uint256 index) public view returns (bytes32) {
+        return _set.at(index);
+    }
+
+    function values() public view returns (bytes32[] memory) {
+        return _set.values();
+    }
+}
+
+// AddressSet
+contract EnumerableAddressSetMock {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    event OperationResult(bool result);
+
+    EnumerableSet.AddressSet private _set;
+
+    function contains(address value) public view returns (bool) {
+        return _set.contains(value);
+    }
+
+    function add(address value) public {
+        bool result = _set.add(value);
+        emit OperationResult(result);
+    }
+
+    function remove(address value) public {
+        bool result = _set.remove(value);
+        emit OperationResult(result);
+    }
+
+    function length() public view returns (uint256) {
+        return _set.length();
+    }
+
+    function at(uint256 index) public view returns (address) {
+        return _set.at(index);
+    }
+
+    function values() public view returns (address[] memory) {
+        return _set.values();
+    }
+}
+
+// UintSet
+contract EnumerableUintSetMock {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    event OperationResult(bool result);
+
+    EnumerableSet.UintSet private _set;
+
+    function contains(uint256 value) public view returns (bool) {
+        return _set.contains(value);
+    }
+
+    function add(uint256 value) public {
+        bool result = _set.add(value);
+        emit OperationResult(result);
+    }
+
+    function remove(uint256 value) public {
+        bool result = _set.remove(value);
+        emit OperationResult(result);
+    }
+
+    function length() public view returns (uint256) {
+        return _set.length();
+    }
+
+    function at(uint256 index) public view returns (uint256) {
+        return _set.at(index);
+    }
+
+    function values() public view returns (uint256[] memory) {
+        return _set.values();
+    }
+}
